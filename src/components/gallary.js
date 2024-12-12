f230e1eef57dda872ee05c99345d3ef7e3635472
jeevajeyakrishnan <jeeevasuriya@gmail.com>
2024-12-12 21:05:45 +0530

    first-commit

---
+++ src/components/gallary.js
@@ -0,0 +1,11 @@
+export default function Gallary(){
+    return(
+        <>
+            <div>
+                <div className="backgound">
+                    <img src={img} alt="background" />
+                </div>  
+            </div>
+        </>
+    )
+}
